471fdda8c24590308f9edb79576c643e806d7c99
Pedro Henrique <pedrops02@gmail.com>
2018-10-17 12:32:46 -0300

    Update conexao.php

--- automatic-irrigation/conexao.php
+++ automatic-irrigation/conexao.php
@@ -3,7 +3,7 @@
     // constantes com as credenciais de acesso ao banco MySQL
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', 'pedrops03');
+    define('DB_PASS', '');
     define('DB_NAME', 'planta');
 
     // habilita todas as exibições de erros
