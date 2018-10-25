f827e02174caf3474f1f7f3b612129af8b75ea5f
thedrops <pedrops02@gmail.com>
2018-10-24 22:04:43 -0200

    nav

--- automatic-irrigation/include/nav.php
+++ automatic-irrigation/include/nav.php
@@ -2,13 +2,13 @@
     <div class="nav-wrapper container">
       <a id="logo-container" href="index.php" class="brand-logo">Wetter</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="planta-form.php">Cadastro</a></li>
+        <li><a href="cadastro-planta.php">Cadastro</a></li>
         <li><a href="consulta-planta.php">Consulta</a></li>
         <li><a class=" modal-trigger" href="#modal1">Login</a>
       </ul>
 
       <ul id="nav-mobile" class="sidenav">
-        <li><a href="planta-form.php">Cadastro</a></li>
+        <li><a href="cadastro-planta.php">Cadastro</a></li>
         <li><a href="consulta-planta.php">Consulta</a></li>
       </ul>
       <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
