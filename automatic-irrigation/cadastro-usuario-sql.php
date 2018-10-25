b321135a5b6d78f8322da52ff0c57ae7fc3a22e0
thedrops <pedrops02@gmail.com>
2018-10-24 22:31:21 -0200

    alterações cadastro usuário

--- automatic-irrigation/cadastro-usuario-sql.php
+++ automatic-irrigation/cadastro-usuario-sql.php
@@ -1,11 +1,13 @@
 <?php
+    include('include/header.php');
+    include('include/nav.php');
     require_once 'conexao.php';
     $email = isset($_POST['email']) ? $_POST['email'] : null;
     $nome = isset($_POST['nome_usuario']) ? $_POST['nome_usuario'] : null;
     $login = isset($_POST['nome_login']) ? $_POST['nome_login'] : null;
     $endereco = isset($_POST['endereco']) ? $_POST['endereco'] : null;
     $senha = sprintf('%07X', mt_rand(0, 0xFFFFFFF));
-    echo($senha);
+    echo("Senha: "+$senha);
     $senha = sha1(md5($senha));
     if(empty($login)){
         echo "Informe o usuário";
@@ -22,9 +24,11 @@
     $stmt->bindParam(':senha', $senha);
 
     if($stmt->execute()){
-        echo "Usuário gerado com sucesso"; 
+        echo "<h1>Usuário gerado com sucesso</h1>";
     }
     else{
         echo "Não foi possível gerar um usuário";
     }
+
+    include('include/footer.php');
 ?>
