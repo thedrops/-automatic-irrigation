5076a6acbbe9440a8c65c8376724718f300ff4b6
Rebeca Castilho Camargo <becacastilhocamargo@gmail.com>
2018-10-25 22:06:03 -0300

    Add files via upload
    index modificado

--- automatic-irrigation/index.php
+++ automatic-irrigation/index.php
@@ -5,13 +5,13 @@ include('include/nav.php');
         <!-- Header-->
 
 
-  <div id="index-banner" class="parallax-container">
+  <div style="background-color: rgba(174, 211, 255, 0.2);" id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
       <div class="container">
         <br><br>
-        <h1 class="header center white-text text-lighten-2">Wetter Irrigator</h1>
+        <h1 class="header center white-text text-lighten-2">Monitore seu plantil</h1>
         <div class="row center">
-          <h5 class="header col s12 light">Plataforma de controle</h5>
+          <h5 class="header col s12 " >Plataforma de controle de irrigação de precisão</h5>
         </div>
         <div class="row center">
           <a href="#informacoes"  class="btn-large  teal lighten-1">Confira</a>
@@ -31,28 +31,28 @@ include('include/nav.php');
       <div class="row">
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center teal-text"><i class="material-icons">flash_on</i></h2>
-            <h5 class="center">Speeds up development</h5>
+            <h2 class="center blue-text"><i class="material-icons">opacity</i></h2>
+            <h5 class="center">Economia de água</h5>
 
-            <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+            <p style="text-align:justify">Através de um algoritmo elaborado de medição de temperatura e umidade que controla a rega obtem se a maior eficiência no consumo de água.</p>
           </div>
         </div>
 
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center teal-text"><i class="material-icons">group</i></h2>
-            <h5 class="center">User Experience Focused</h5>
+          <h2 class="center teal-text"><i class="material-icons">attach_money</i></h2>
+            <h5 class="center">Baixo Custo</h5>
 
-            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+            <p>By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
           </div>
         </div>
 
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center teal-text"><i class="material-icons">settings</i></h2>
-            <h5 class="center">Easy to work with</h5>
+            <h2 class="center blue-text"><i class="material-icons">brush</i></h2>
+            <h5 class="center">Customização</h5>
 
-            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+            <p style="text-align:justify">O Sistema vai permitir a adção de novas funções, como sistema autonomo de energia ou alternar o tipo de alimentação baseado na pressão da água.</p>
           </div>
         </div>
       </div>
