930126d1e0f11647d3f77bdcb7128aea919136a3
Mithrarin <fabianoxtx@gmail.com>
2018-10-26 21:50:11 -0200

    Input senha adicionado e ícones nos inputs reformulados

--- automatic-irrigation/cadastro-usuario.php
+++ automatic-irrigation/cadastro-usuario.php
@@ -9,34 +9,46 @@
         <form class="col s12" method="POST" action="cadastro-usuario-sql.php">
           <div class="row">
             <div class="input-field col s6">
-              <input name="nome_usuario" placeholder="ex: Delano de Souza Morais" id="nome_usuario" type="text" class="validate">
-              <label for="nome_usuario">Nome Completo</label>
+                <i class="material-icons prefix">person</i>
+                <input name="nome_usuario" placeholder="ex: Delano de Souza Morais" id="nome_usuario" type="text"  class="validate">
+              <label for="nome_usuario" class='black-text input-label'><b>Nome Completo</b></label>
             </div>
 
 
               <div class="row">
                 <div class="input-field col s6">
+                  <i class="material-icons prefix">email</i>
                   <input placeholder="ex: Wetter@company.com" name="email" id="email" type="text" class="validate">
-                  <label for="email">E-mail</label>
+                  <label for="email" class='black-text'><b>E-mail</b></label>
                 </div>
           </div>
           </div>
           <div class="row">
             <div class="input-field col s6">
+              <i class="material-icons prefix">account_box</i>
               <input name="nome_login" placeholder="ex: Bombomzinho" id="nome_login" type="text" class="validate">
-              <label for="nome_login">Nome de Usuário</label>
+              <label for="nome_login" class='black-text'><b>Nome de Usuário</b></label>
             </div>
-
-              <div class="row">
+            <div class="row">
                 <div class="input-field col s6">
-                  <input name="endereco"placeholder="ex: Rua Oswaldo Cruz, nº 10, centro, Caraguatatuba" id="endereco" type="text" class="validate">
-                  <label for="endereco">Endereço</label>
+                   <i class="material-icons prefix">lock</i>
+                   <input name="senha"placeholder="***" id="senha" type="password" class="validate">
+                  <label for="senha" class='black-text'><b>Senha de Usuário<b/></label>
                 </div>
-          </div>
+              </div>
+
+
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">home</i>
+                    <input name="endereco"placeholder="ex: Rua Oswaldo Cruz, nº 10, centro, Caraguatatuba" id="endereco" type="text" class="validate">
+                  <label for="endereco" class='black-text'><b>Endereço<b/></label>
+                </div>
+
+
           </div>
 
             <button class="btn waves-effect waves-light right" type="submit" name="action">Solicitar
-                <i class="material-icons right">add_shopping_cart</i>
+                <i class="material-icons right">check</i>
             </button>
 
 
