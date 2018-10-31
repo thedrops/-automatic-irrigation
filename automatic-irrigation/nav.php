33f20b247f1ed0d86647b187ab12d9bee3a618ef
Pedro Henrique <pedrops02@gmail.com>
2018-10-31 20:27:12 -0300

    Login

---
+++ automatic-irrigation/nav.php
@@ -0,0 +1,18 @@
+<nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="index.php" class="brand-logo"><img src='images/WETTER.png'/></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="cadastro-planta.php">Cadastro</a></li>
+        <li><a href="consulta-planta.php">Consulta</a></li>
+        <li><a class=" modal-trigger" href="#modal1">Login</a>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="cadastro-planta.php">Cadastro</a></li>
+        <li><a href="consulta-planta.php">Consulta</a></li>
+        <li><a class=" modal-trigger" href="#modal1">Login</a>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+<main>
