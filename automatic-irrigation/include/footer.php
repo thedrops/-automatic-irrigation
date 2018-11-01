7e871101743657c2bb8d70006d8fcaa05cf4d165
thedrops <pedrops02@gmail.com>
2018-10-31 22:14:04 -0200

    Login ok

--- automatic-irrigation/include/footer.php
+++ automatic-irrigation/include/footer.php
@@ -1,5 +1,29 @@
 </main>
-<footer class="page-footer teal ">
+<!-- Modal Structure -->
+  <div id="modal1" class="modal" style="width:30%">
+    <div class="modal-content">
+      <h4 class=' center'>Cadastro</h4>
+
+    <form class="col s12" action="verifica-login.php" >
+      <div class="row center">
+        <div class="input-field col s12">
+          <input placeholder="Ex:teste@teste" id="email" type="text" class="validate">
+          <label for="email">E-mail</label>
+        </div>
+        </div>
+        <div class="row">
+        <div class="input-field col s12">
+          <input placeholder="Ex:123" id="senha" type="password" class="validate">
+          <label for="senha">Senha</label>
+        </div>
+        </div>
+        <button class="btn waves-effect waves-light  cadastro-usuario" >Logar
+                <i class="material-icons right">send</i>
+        </button>
+    </form>
+  </div>
+</div>
+ <footer class="page-footer teal ">
             <div class="row conta container">
               <div class="col l6 s12">
                 <p class="grey-text text-lighten-4">Você pode conferir o nosso repositório.</p>
@@ -20,10 +44,8 @@
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
+   // Or with jQuery
+   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
 </script>
@@ -32,10 +54,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems, options);
   });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
+   // Or with jQuery
+   $(document).ready(function(){
     $('.modal').modal();
   });
 </script>
--- automatic-irrigation/include/nav.php
+++ automatic-irrigation/include/nav.php
@@ -4,11 +4,12 @@
       <ul class="right hide-on-med-and-down">
         <li><a href="cadastro-planta.php">Cadastro</a></li>
         <li><a href="consulta-planta.php">Consulta</a></li>
+        <li><a class=" modal-trigger" href="#modal1">Login</a>
       </ul>
-
-      <ul id="nav-mobile" class="sidenav">
+       <ul id="nav-mobile" class="sidenav">
         <li><a href="cadastro-planta.php">Cadastro</a></li>
         <li><a href="consulta-planta.php">Consulta</a></li>
+        <li><a class=" modal-trigger" href="#modal1">Login</a>
       </ul>
       <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
