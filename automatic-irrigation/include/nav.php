6bcbed57458794622f6c41424e11abda1494b1e7
Rebeca Castilho Camargo <becacastilhocamargo@gmail.com>
2018-11-09 20:08:38 -0200

    Add files via upload
    navbar admin

--- automatic-irrigation/include/nav.php
+++ automatic-irrigation/include/nav.php
@@ -1,5 +1,6 @@
 <?php
 if(!$_SESSION){ ?>
+
 <nav class="white" role="navigation">
     <div class="nav-wrapper container">
       <a id="logo-container" href="index.php" class="brand-logo"><img src='images/WETTER.png'/></a>
@@ -16,15 +17,16 @@ if(!$_SESSION){ ?>
 
 <?php }else{ ?>
 
-    <nav class="white" role="navigation">
-    <div class="nav-wrapper container">
-      <a id="logo-container" href="painel.php" class="brand-logo"><img src='images/WETTER.png'/></a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="cadastro-planta.php">Cadastro</a></li>
-        <li><a href="consulta-planta.php">Consulta</a></li>
-        <li><a href="grafico.php">Gráfico</a></li>
-        <li><a href="logout.php">Logout</a>
-      </ul>
+  <ul id="nav-mobile" class="sidenav sidenav-fixed" style="transform: translateX(0%);">
+        <li  style="margin-top:10%;margin-left:25%;" href="painel.php" class="brand-logo"><img src='images/WETTER.png'/></li>
+        <li class="bold"><a href="cadastro-planta.php" class="waves-effect waves-teal">Cadastro de Plantas</a></li>
+        <li class="bold"><a href="consulta-planta.php" class="waves-effect waves-teal">Consulta de Plantas</a></li>
+        <li class="bold"><a href="grafico.php" class="waves-effect waves-teal">Grafico de Regas</a></li>
+        <li class="bold"><a href="logout.php" class="waves-effect waves-teal">Logout</a></li>
+    </ul>
+
+    
+
        <ul id="nav-mobile" class="sidenav">
         <li><a href="cadastro-planta.php">Cadastro</a></li>
         <li><a href="consulta-planta.php">Consulta</a></li>
@@ -33,6 +35,7 @@ if(!$_SESSION){ ?>
       </ul>
       <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
-  </nav>
+ 
 <main>
+
     <?php } ?>
