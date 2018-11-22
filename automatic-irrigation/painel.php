d8d69af30a5114e445e6b78c55e642858c42aa5f
Fabiano Pereira <fabianoxtx@gmail.com>
2018-11-22 18:22:35 -0200

    correção

--- automatic-irrigation/painel.php
+++ automatic-irrigation/painel.php
@@ -5,5 +5,5 @@ require_once 'painel-sql.php';
 
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
-<h1 class='center'>Bem Vindo!<?php echo $user['nome_usuario'];?> </h1>
+<h1 class='center'>Bem-vindo!<?php echo $user['nome_usuario'];?> </h1>
 
